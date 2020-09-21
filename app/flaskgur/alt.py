e14a8bdbf99f9f92e560e23a6433dd20f2212d8b
Matthew Maitland <mmaitland300@gmail.com>
2020-09-20 23:51:23 -0600

    updated requirements

--- app/flaskgur/alt.py
+++ app/flaskgur/alt.py
@@ -44,115 +44,6 @@ import matplotlib.pyplot as plt
 import scipy
 import copy
 
-class PyGram:
-	
-	def __init__(self, filename):
-		self.filename = filename
-		self.im = False
-		
-	def image(self):
-		if not self.im:
-			self.im = PIL.Image.open(self.filename)
-		return self.im
-	
-	def execute(self, command, **kwargs):
-		default = dict(
-			filename=self.filename,
-			width = self.image().size[0],
-			height = self.image().size[1]
-		)
-		format = dict(default.items() | kwargs.items())
-		command = command.format(**format)
-		error = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-		return error
-		
-	def colortone(self, color, level, type = 0):
-		
-		arg0 = level
-		arg1 = 100 - level
-		if type == 0:
-			negate = '-negate'
-		else:
-			negate = ''
-
-		self.execute("convert {filename} \( -clone 0 -fill '{color}' -colorize 100% \) \( -clone 0 -colorspace gray {negate} \) -compose blend -define compose:args={arg0},{arg1} -composite {filename}",
-			color = color,
-			negate = negate,
-			arg0 = arg0,
-			arg1 = arg1
-		)
-
-# Decorations
-
-class Vignette(PyGram):
-    def vignette(self, color_1='none', color_2='black', crop_factor=1.5):
-        crop_x = math.floor(self.image().size[0] * crop_factor)
-        crop_y = math.floor(self.image().size[1] * crop_factor)
-
-        self.execute("convert \( {filename} \) \( -size {crop_x}x{crop_y} radial-gradient:{color_1}-{color_2} -gravity center -crop {width}x{height}+0+0 +repage \) -compose multiply -flatten {filename}",
-			crop_x = crop_x,
-			crop_y = crop_y,
-			color_1 = color_1,
-			color_2 = color_2,
-		)
-
-
-
-class Frame(PyGram):
-    def frame(self, frame):
-        path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        self.execute(
-            "convert {filename} \( '{frame}' -resize {width}x{width}! -unsharp 1.5x1.0+1.5+0.02 \) -flatten {filename}",
-            frame=os.path.join(path, "frames", frame)
-        )
-
-class Border(PyGram):
-    def border(self, color='black', width=20):
-        self.execute("convert {filename} -bordercolor {color} -border {bwidth}x{bwidth} {filename}",
-                     color=color,
-                     bwidth=width
-        )
-
-
-# Filters
-
-class Toaster(Vignette, Border):
-    def apply(self):
-        self.colortone('#330000', 50, 0)
-        self.execute("convert {filename} -modulate 150,80,100 -gamma 1.2 -contrast -contrast {filename}")
-        self.vignette('none', 'LavenderBlush3')
-        self.vignette('#ff9966', 'none')
-        self.border('white')
-
-class Lomo(Vignette):
-
-	def apply(self):
-		self.execute("convert {filename} -channel R -level 33% -channel G -level 33% {filename}")
-		self.vignette()
-
-class Kelvin(Border):
-
-	def apply(self):
-		self.execute(
-            "convert \( {filename} -auto-gamma -modulate 120,50,100 \) \( -size {width}x{height} -fill 'rgba(255,153,0,0.5)' -draw 'rectangle 0,0 {width},{height}' \) -compose multiply {filename}")
-		self.border()
-
-class Gotham(Border):
-    def apply(self):
-        self.execute(
-            "convert {filename} -modulate 120,10,100 -fill '#222b6d' -colorize 20 -gamma 0.5 -contrast -contrast {filename}")
-        self.border()
-
-class Nashville(Border):
-
-	def apply(self):
-		self.colortone('#222b6d', 50, 0)
-		self.colortone('#f7daae', 120, 1)
-		self.execute("convert {filename} -contrast -modulate 100,150,100 -auto-gamma {filename}")
-		self.border()
-# x = Lomo("sharp.jpg")
-# x.apply()
-
 def edge(image):
     im = cv2.imread(image)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -410,3 +301,113 @@ def mainquad(filename, left=None, right=None, up=None, down=None, size=512, draw
     
     quadart.save(filename)
 
+
+
+# class PyGram:
+	
+# 	def __init__(self, filename):
+# 		self.filename = filename
+# 		self.im = False
+		
+# 	def image(self):
+# 		if not self.im:
+# 			self.im = PIL.Image.open(self.filename)
+# 		return self.im
+	
+# 	def execute(self, command, **kwargs):
+# 		default = dict(
+# 			filename=self.filename,
+# 			width = self.image().size[0],
+# 			height = self.image().size[1]
+# 		)
+# 		format = dict(default.items() | kwargs.items())
+# 		command = command.format(**format)
+# 		error = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+# 		return error
+		
+# 	def colortone(self, color, level, type = 0):
+		
+# 		arg0 = level
+# 		arg1 = 100 - level
+# 		if type == 0:
+# 			negate = '-negate'
+# 		else:
+# 			negate = ''
+
+# 		self.execute("convert {filename} \( -clone 0 -fill '{color}' -colorize 100% \) \( -clone 0 -colorspace gray {negate} \) -compose blend -define compose:args={arg0},{arg1} -composite {filename}",
+# 			color = color,
+# 			negate = negate,
+# 			arg0 = arg0,
+# 			arg1 = arg1
+# 		)
+
+# # Decorations
+
+# class Vignette(PyGram):
+#     def vignette(self, color_1='none', color_2='black', crop_factor=1.5):
+#         crop_x = math.floor(self.image().size[0] * crop_factor)
+#         crop_y = math.floor(self.image().size[1] * crop_factor)
+
+#         self.execute("convert \( {filename} \) \( -size {crop_x}x{crop_y} radial-gradient:{color_1}-{color_2} -gravity center -crop {width}x{height}+0+0 +repage \) -compose multiply -flatten {filename}",
+# 			crop_x = crop_x,
+# 			crop_y = crop_y,
+# 			color_1 = color_1,
+# 			color_2 = color_2,
+# 		)
+
+
+
+# class Frame(PyGram):
+#     def frame(self, frame):
+#         path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+#         self.execute(
+#             "convert {filename} \( '{frame}' -resize {width}x{width}! -unsharp 1.5x1.0+1.5+0.02 \) -flatten {filename}",
+#             frame=os.path.join(path, "frames", frame)
+#         )
+
+# class Border(PyGram):
+#     def border(self, color='black', width=20):
+#         self.execute("convert {filename} -bordercolor {color} -border {bwidth}x{bwidth} {filename}",
+#                      color=color,
+#                      bwidth=width
+#         )
+
+
+# # Filters
+
+# class Toaster(Vignette, Border):
+#     def apply(self):
+#         self.colortone('#330000', 50, 0)
+#         self.execute("convert {filename} -modulate 150,80,100 -gamma 1.2 -contrast -contrast {filename}")
+#         self.vignette('none', 'LavenderBlush3')
+#         self.vignette('#ff9966', 'none')
+#         self.border('white')
+
+# class Lomo(Vignette):
+
+# 	def apply(self):
+# 		self.execute("convert {filename} -channel R -level 33% -channel G -level 33% {filename}")
+# 		self.vignette()
+
+# class Kelvin(Border):
+
+# 	def apply(self):
+# 		self.execute(
+#             "convert \( {filename} -auto-gamma -modulate 120,50,100 \) \( -size {width}x{height} -fill 'rgba(255,153,0,0.5)' -draw 'rectangle 0,0 {width},{height}' \) -compose multiply {filename}")
+# 		self.border()
+
+# class Gotham(Border):
+#     def apply(self):
+#         self.execute(
+#             "convert {filename} -modulate 120,10,100 -fill '#222b6d' -colorize 20 -gamma 0.5 -contrast -contrast {filename}")
+#         self.border()
+
+# class Nashville(Border):
+
+# 	def apply(self):
+# 		self.colortone('#222b6d', 50, 0)
+# 		self.colortone('#f7daae', 120, 1)
+# 		self.execute("convert {filename} -contrast -modulate 100,150,100 -auto-gamma {filename}")
+# 		self.border()
+# # x = Lomo("sharp.jpg")
+# # x.apply()
